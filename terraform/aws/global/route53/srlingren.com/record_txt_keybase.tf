9a92de5945c720b7738d0832a9dd91bf377e9f85
Sean Lingren <srlingren@gmail.com>
2018-10-25 23:59:48 -0400

    keybase records

--- terraform/aws/global/route53/srlingren.com/record_txt_keybase.tf
+++ terraform/aws/global/route53/srlingren.com/record_txt_keybase.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "txt_keybase" {
   zone_id = "${ aws_route53_zone.zone.zone_id }"
-  name    = "@"
+  name    = "_keybase"
   type    = "TXT"
   ttl     = "${ module.constants.route53_default_txt_ttl }"
   records = ["keybase-site-verification=bGi1F-LK1JuddMPjZlB84NL6kvSmQ1uK2Us0r1SMt1c"]
--- terraform/aws/global/route53/vaku.io/record_txt_keybase.tf
+++ terraform/aws/global/route53/vaku.io/record_txt_keybase.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "txt_keybase" {
   zone_id = "${ aws_route53_zone.zone.zone_id }"
-  name    = "@"
+  name    = "_keybase"
   type    = "TXT"
   ttl     = "${ module.constants.route53_default_txt_ttl }"
   records = ["keybase-site-verification=8EUL92yKoed8HeJs4zrZiupbo_S4zZw1bxomxDnOHlQ"]
