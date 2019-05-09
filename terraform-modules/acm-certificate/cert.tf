9a589fd3fc7e45c8c071bae9325bc5d7bda42d80
Sean Lingren <srlingren@gmail.com>
2019-05-08 21:50:16 -0400

    acm-certificate

--- terraform-modules/acm-certificate/cert.tf
+++ terraform-modules/acm-certificate/cert.tf
@@ -1,18 +1,26 @@
 resource "aws_acm_certificate" "cert" {
-  provider = "aws.cert"
+  provider = aws.cert
 
-  domain_name = "${ local.dns_names[0] }"
+  domain_name = local.dns_names[0]
 
-  validation_method         = "DNS"
-  subject_alternative_names = ["${ slice( local.dns_names, 1, length(local.dns_names) ) }"]
+  validation_method = "DNS"
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  subject_alternative_names = [slice(local.dns_names, 1, length(local.dns_names))]
 
-  tags = "${ merge(
-    map("Name", "${ replace( local.dns_names[0], "*", "star" ) }"),
-    map("fqdn", "${ replace( local.dns_names[0], "*", "star" ) }"),
-    map("sans", "${ replace( join( " / ", slice( local.dns_names, 1, length(local.dns_names) ) ), "*", "star" ) }"),
-    map("valid_domains", "${ replace( join( " / ", local.dns_names ), "*", "star" ) }"),
-    var.tags )
-  }"
+  tags = merge(
+    {"Name" = replace(local.dns_names[0], "*", "star")},
+    {"fqdn" = replace(local.dns_names[0], "*", "star")},
+    {"sans" = replace(join(" / ", slice(local.dns_names, 1, length(local.dns_names))),"*","star",)},
+    {"valid_domains" = replace(join(" / ", local.dns_names), "*", "star")},
+    var.tags,
+  )
 
   lifecycle {
     create_before_destroy = true
@@ -27,16 +35,24 @@ resource "aws_acm_certificate" "cert" {
 # you will really only need to create ONE record. You can either deduplicate the domain_validation
 # list or you can use allow_overwrite which lets terraform think that both records exist
 resource "aws_route53_record" "cert" {
-  provider = "aws.dns"
+  provider = aws.dns
 
-  count           = "${ length(local.dns_names) }"
+  count           = length(local.dns_names)
   allow_overwrite = true
 
-  zone_id = "${ data.aws_route53_zone.zone.*.zone_id[count.index] }"
-  name    = "${ lookup( aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_name" ) }"
-  type    = "${ lookup( aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_type" ) }"
+  zone_id = data.aws_route53_zone.zone[count.index].zone_id
+  name    = aws_acm_certificate.cert.domain_validation_options[count.index]["resource_record_name"]
+  type    = aws_acm_certificate.cert.domain_validation_options[count.index]["resource_record_type"]
   ttl     = 60
-  records = ["${ lookup( aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_value" ) }"]
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  records = [aws_acm_certificate.cert.domain_validation_options[count.index]["resource_record_value"]]
 
   lifecycle {
     create_before_destroy = true
@@ -44,10 +60,10 @@ resource "aws_route53_record" "cert" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  provider = "aws.cert"
+  provider = aws.cert
 
-  certificate_arn         = "${ aws_acm_certificate.cert.arn }"
-  validation_record_fqdns = ["${ aws_route53_record.cert.*.fqdn }"]
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = aws_route53_record.cert.*.fqdn
 
   lifecycle {
     create_before_destroy = true
--- terraform-modules/acm-certificate/variables.tf
+++ terraform-modules/acm-certificate/variables.tf
@@ -1,9 +1,9 @@
 variable "dns_names_to_zone_names" {
-  type        = "map"
+  type        = map(string)
   description = "A map of FQDNs to their Route53 Zone names that this site should be accessible at. Ex: ['example.com' => 'example.com', 'foo.example.com' => 'foo.example.com', '*.bar.example.com' => 'bar.example.com']"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   description = "A map of tags to apply to all resources"
 }
