bf34f5ccc283706f43efb832d348bda899a9a239
Sean Lingren <srlingren@gmail.com>
2019-05-08 21:47:08 -0400

    route53-zone-delegation

--- terraform-modules/route53-zone-delegation/route53.tf
+++ terraform-modules/route53-zone-delegation/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "delegation" {
-  zone_id = "${ var.zone_id }"
-  name    = "${ var.domain }"
+  zone_id = var.zone_id
+  name    = var.domain
   type    = "NS"
   ttl     = 3600
-  records = ["${ var.nameservers }"]
+  records = var.nameservers
 }
--- terraform-modules/route53-zone-delegation/variables.tf
+++ terraform-modules/route53-zone-delegation/variables.tf
@@ -1,14 +1,14 @@
 variable "zone_id" {
-  type        = "string"
+  type        = string
   description = "The hosted zone ID of the zone that will be doing the delegation"
 }
 
 variable "domain" {
-  type        = "string"
+  type        = string
   description = "The domain to delegate (ex: dev.example.com)"
 }
 
 variable "nameservers" {
-  type        = "list"
+  type        = list(string)
   description = "The list of nameservers of the route53 zone that we are delegating to"
 }
