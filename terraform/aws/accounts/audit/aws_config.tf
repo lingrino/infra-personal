88e43fad62f7c5635e8b152541b5bb2e619375e9
Sean Lingren <srlingren@gmail.com>
2019-08-01 16:09:56 -0400

    rm unnecessary role

--- terraform/aws/accounts/audit/aws_config.tf
+++ terraform/aws/accounts/audit/aws_config.tf
@@ -10,40 +10,4 @@ resource "aws_config_configuration_aggregator" "lingrino" {
     { "Name" = "lingrino" },
     var.tags
   )
-
-  depends_on = [aws_iam_role_policy_attachment.config_lingrino_aggregator_service]
-}
-
-resource "aws_iam_role" "config_lingrino_aggregator" {
-  name_prefix = "config-lingrino-aggregator-"
-  description = "Allow AWS Config to get information about the AWS Organization"
-
-  assume_role_policy = data.aws_iam_policy_document.arp_config_lingrino_aggregator.json
-
-  tags = merge(
-    { "Name" = "config-lingrino-aggregator" },
-    { "description" = "Allow AWS Config to get information about the AWS Organization" },
-    var.tags
-  )
-}
-
-resource "aws_iam_role_policy_attachment" "config_lingrino_aggregator_service" {
-  role       = aws_iam_role.config_lingrino_aggregator.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRoleForOrganizations"
-}
-
-data "aws_iam_policy_document" "arp_config_lingrino_aggregator" {
-  statement {
-    sid    = "AllowConfigAssume"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["config.amazonaws.com"]
-    }
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-  }
 }
