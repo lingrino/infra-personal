25d7df4fbdc0d3c1a5323d66e511e65881db59bb
Sean Lingren <srlingren@gmail.com>
2019-08-01 23:00:33 -0400

    kms cloudtrail

--- terraform/aws/accounts/root/cloudtrail.tf
+++ terraform/aws/accounts/root/cloudtrail.tf
@@ -2,6 +2,8 @@ resource "aws_cloudtrail" "organization" {
   name           = "lingrino"
   enable_logging = true
 
+  kms_key_id = data.terraform_remote_state.account_audit.outputs.kms_key_cloudtrail_arn
+
   s3_bucket_name = data.terraform_remote_state.account_audit.outputs.bucket_cloudtrail_name
 
   is_multi_region_trail         = true
