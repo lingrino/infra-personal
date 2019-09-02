630b1585ad4f30ec5dfbaacd74e146c0e8053738
Sean Lingren <srlingren@gmail.com>
2019-09-02 10:04:41 -0500

    list

--- terraform-modules/static-site/healthcheck.tf
+++ terraform-modules/static-site/healthcheck.tf
@@ -1,7 +1,7 @@
 module "healthcheck" {
   source = "../route53-healthcheck//"
 
-  domains = var.healthcheck_cloudfront_enabled ? merge(local.healthcheck_domains, aws_cloudfront_distribution.cf.domain_name) : local.healthcheck_domains
+  domains = var.healthcheck_cloudfront_enabled ? merge(local.healthcheck_domains, [aws_cloudfront_distribution.cf.domain_name]) : local.healthcheck_domains
 
   sns_arn = var.healthcheck_sns_arn
 
