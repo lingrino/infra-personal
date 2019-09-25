2d5156e1becf64199bef91f74233f383ff847849
Sean Lingren <sean@lingrino.com>
2019-09-24 21:28:06 -0700

    add-vpcs

---
+++ terraform/aws/accounts/prod/vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "../../../../terraform-modules/vpc//"
+
+  name_prefix    = "prod"
+  vpc_cidr_block = "10.10.0.0/16"
+
+  # NAT Gateways are too expensive for my personal use
+  create_nat_gateways = false
+
+  azs = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+    "us-east-1d",
+  ]
+
+  tags = var.tags
+}
