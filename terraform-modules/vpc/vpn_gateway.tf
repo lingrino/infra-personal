16d2a64b6666b4a56acc8bedefae3ef44661498a
Sean Lingren <sean@lingrino.com>
2020-02-03 15:38:50 -0600

    moreeaches

--- terraform-modules/vpc/vpn_gateway.tf
+++ terraform-modules/vpc/vpn_gateway.tf
@@ -13,19 +13,19 @@ resource "aws_vpn_gateway_route_propagation" "public" {
   count = var.create_vpn_gateway ? 1 : 0
 
   route_table_id = aws_route_table.public.id
-  vpn_gateway_id = aws_vpn_gateway.vpn[0].id
+  vpn_gateway_id = aws_vpn_gateway.vpn.0.id
 }
 
 resource "aws_vpn_gateway_route_propagation" "private" {
-  count = var.create_vpn_gateway ? length(var.azs) : 0
+  for_each = var.create_vpn_gateway ? toset(aws_route_table.private) : {}
 
-  route_table_id = aws_route_table.private.*.id[count.index]
-  vpn_gateway_id = aws_vpn_gateway.vpn[0].id
+  route_table_id = each.value.id
+  vpn_gateway_id = aws_vpn_gateway.vpn.0.id
 }
 
 resource "aws_vpn_gateway_route_propagation" "intra" {
-  count = var.create_vpn_gateway ? length(var.azs) : 0
+  for_each = var.create_vpn_gateway ? toset(aws_route_table.intra) : {}
 
-  route_table_id = aws_route_table.intra.*.id[count.index]
-  vpn_gateway_id = aws_vpn_gateway.vpn[0].id
+  route_table_id = each.value.id
+  vpn_gateway_id = aws_vpn_gateway.vpn.0.id
 }
--- terraform-modules/wireguard/route53.tf
+++ terraform-modules/wireguard/route53.tf
@@ -5,7 +5,7 @@ data "aws_route53_zone" "wg" {
 
 resource "aws_route53_record" "wg" {
   count   = var.zone_name == "" || var.domain_name == "" ? 0 : 1
-  zone_id = data.aws_route53_zone.wg[count.index].zone_id
+  zone_id = data.aws_route53_zone.wg.0.zone_id
   name    = var.domain_name
   type    = "A"
   ttl     = 60
