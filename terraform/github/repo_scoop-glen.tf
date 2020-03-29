9c4a569f55fe33cf751c98a75bc3fe033f4544a8
Sean Lingren <sean@lingrino.com>
2020-03-29 12:21:25 -0500

    glen and cami

---
+++ terraform/github/repo_scoop-glen.tf
@@ -0,0 +1,20 @@
+resource "github_repository" "scoop-glen" {
+  name         = "scoop-glen"
+  description  = "Scoop bucket for glen binaries. Powered by @goreleaser"
+  homepage_url = "https://lingrino.com/"
+
+  default_branch = "master"
+  private        = false
+
+  has_wiki   = false
+  has_issues = false
+
+  allow_merge_commit = true
+  allow_squash_merge = true
+  allow_rebase_merge = true
+
+  topics = [
+    "scoop",
+    "glen",
+  ]
+}
