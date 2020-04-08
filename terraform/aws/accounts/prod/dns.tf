aa4ebcf446c430c086ac20570b06a63055d6c127
Sean Lingren <sean@lingrino.com>
2020-04-07 22:52:13 -0500

    data soruce

--- terraform/aws/accounts/prod/dns.tf
+++ terraform/aws/accounts/prod/dns.tf
@@ -1,6 +1,10 @@
+data "aws_route53_zone" "lingrino_dev" {
+  name = "lingrino.dev"
+}
+
 # Hostnames for tailscale devices
 resource "aws_route53_record" "pi_lingrino_dev" {
-  zone_id = module.zone_lingrino_dev.zone_id
+  zone_id = data.aws_route53_zone.lingrino_dev.zone_id
   name    = "pi.lingrino.dev."
   type    = "A"
   ttl     = 300
@@ -8,7 +12,7 @@ resource "aws_route53_record" "pi_lingrino_dev" {
 }
 
 resource "aws_route53_record" "adguard_lingrino_dev" {
-  zone_id = module.zone_lingrino_dev.zone_id
+  zone_id = data.aws_route53_zone.lingrino_dev.zone_id
   name    = "adguard.lingrino.dev."
   type    = "A"
   ttl     = 300
@@ -16,7 +20,7 @@ resource "aws_route53_record" "adguard_lingrino_dev" {
 }
 
 resource "aws_route53_record" "phone_lingrino_dev" {
-  zone_id = module.zone_lingrino_dev.zone_id
+  zone_id = data.aws_route53_zone.lingrino_dev.zone_id
   name    = "phone.lingrino.dev."
   type    = "A"
   ttl     = 300
@@ -24,7 +28,7 @@ resource "aws_route53_record" "phone_lingrino_dev" {
 }
 
 resource "aws_route53_record" "work_lingrino_dev" {
-  zone_id = module.zone_lingrino_dev.zone_id
+  zone_id = data.aws_route53_zone.lingrino_dev.zone_id
   name    = "work.lingrino.dev."
   type    = "A"
   ttl     = 300
@@ -32,7 +36,7 @@ resource "aws_route53_record" "work_lingrino_dev" {
 }
 
 resource "aws_route53_record" "mac_lingrino_dev" {
-  zone_id = module.zone_lingrino_dev.zone_id
+  zone_id = data.aws_route53_zone.lingrino_dev.zone_id
   name    = "mac.lingrino.dev."
   type    = "A"
   ttl     = 300
