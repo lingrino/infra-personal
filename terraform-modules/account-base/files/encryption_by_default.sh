fbe0aea689348fa682b09f9828021bb3c0c8e62f
Sean Lingren <sean@lingrino.com>
2021-06-28 11:51:58 -0700

    better session name

--- terraform-modules/account-base/files/encryption_by_default.sh
+++ terraform-modules/account-base/files/encryption_by_default.sh
@@ -4,7 +4,7 @@
 
 # Assume a role that can enable encryption by default
 # This script assumes your main provider can assume `OrganizationAccountAccessRole` in all accounts
-CREDS=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/OrganizationAccountAccessRole" --role-session-name vpc-remover | jq -r '.Credentials')
+CREDS=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/OrganizationAccountAccessRole" --role-session-name encryption-by-default | jq -r '.Credentials')
 akid=$(echo "$CREDS" | jq -r '.AccessKeyId')
 sak=$(echo "$CREDS" | jq -r '.SecretAccessKey')
 st=$(echo "$CREDS" | jq -r '.SessionToken')
