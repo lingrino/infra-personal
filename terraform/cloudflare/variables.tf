47ed428035ed64f7753e9f93dd9283f59a3c0a1e
Sean Lingren <sean@lingrino.com>
2024-03-10 13:26:34 -0700

    rm files

--- terraform/cloudflare/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "assume_role_name" {
-  type        = string
-  description = "The name of the role to Assume"
-}
-
-variable "assume_role_session_name" {
-  type        = string
-  description = "What to name the session when assuming the role"
-}
