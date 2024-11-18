6a9e433752f7044c2a1cd67deab9cf77e4ac0dca
Sean Lingren <sean@lingren.com>
2024-11-17 16:47:47 -0800

    new bbkey (#2739)
    * new bbkey
    
    * provider
    
    * vars

--- terraform/b2/meta.tf
+++ terraform/b2/meta.tf
@@ -3,6 +3,19 @@
 #################################
 provider "b2" {}
 
+provider "aws" {
+  region = "us-west-2"
+
+  profile             = !can(var.tfc_aws_dynamic_credentials.aliases["prod"]) ? "prod" : null
+  shared_config_files = try([var.tfc_aws_dynamic_credentials.aliases["prod"].shared_config_file], null)
+
+  default_tags {
+    tags = {
+      terraform = "true"
+    }
+  }
+}
+
 #################################
 ### Terraform                 ###
 #################################
@@ -16,6 +29,9 @@ terraform {
   }
 
   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
     b2 = {
       source = "Backblaze/b2"
     }
