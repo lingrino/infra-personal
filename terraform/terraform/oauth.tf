a65a3b28cfc05b71e30e5b792ac343e750aa49ac
Sean Lingren <sean@lingren.com>
2024-11-18 20:35:36 -0800

    rmfile

--- terraform/terraform/oauth.tf
+++
@@ -1,9 +0,0 @@
-resource "tfe_oauth_client" "github" {
-  name                = "github"
-  organization        = "lingrino"
-  api_url             = "https://api.github.com"
-  http_url            = "https://github.com"
-  oauth_token         = "my-vcs-provider-token"
-  service_provider    = "github"
-  organization_scoped = true
-}
