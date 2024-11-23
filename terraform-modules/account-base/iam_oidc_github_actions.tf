69baeca578fc379a5ce385c167aed1e30692d1dd
Sean Lingren <sean@lingren.com>
2024-11-23 15:06:53 -0800

    use sts for actions oidc client id (#2753)
    * use sts for actions oidc client i
    
    * org tfe provider
    
    * prod aws provider
    
    * secrets aws provider instead
    
    * tfeprovider
    
    * use existing prod provider

--- terraform-modules/account-base/iam_oidc_github_actions.tf
+++ terraform-modules/account-base/iam_oidc_github_actions.tf
@@ -4,7 +4,7 @@ data "tls_certificate" "github_actions" {
 
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["aws.workload.identity"]
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
 
   tags = {
