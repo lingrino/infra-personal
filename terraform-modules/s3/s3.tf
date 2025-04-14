39c47ff3add1ec490fc8e878575908d38fd77933
Sean Lingren <sean@lingren.com>
2025-04-13 21:43:15 -0700

    rbtest

--- terraform-modules/s3/s3.tf
+++ terraform-modules/s3/s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_public_access_block" "s3" {
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 }
 
 # turn off acls
