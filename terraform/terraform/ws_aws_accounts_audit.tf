e3d223f6ed08273504028a4dee19705fc5fde547
Sean Lingren <sean@lingren.com>
2025-06-15 22:14:02 -0700

    remove backblaze (#2876)

--- terraform/terraform/ws_aws_accounts_audit.tf
+++ terraform/terraform/ws_aws_accounts_audit.tf
@@ -9,7 +9,6 @@ resource "tfe_workspace" "aws_accounts_audit" {
   queue_all_runs        = false
   allow_destroy_plan    = false
   file_triggers_enabled = true
-  global_remote_state   = true
 
   vcs_repo {
     identifier     = "lingrino/infra-personal"
@@ -21,6 +20,11 @@ resource "tfe_workspace" "aws_accounts_audit" {
     "terraform/aws/accounts/audit/*.tf",
     "terraform-modules/**/*.tf",
   ]
+}
+
+resource "tfe_workspace_settings" "aws_accounts_audit" {
+  workspace_id        = tfe_workspace.aws_accounts_audit.id
+  global_remote_state = true
 }
 
 resource "tfe_workspace_variable_set" "aws_accounts_audit" {
