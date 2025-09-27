0632afd560b9da8776ec3b446f8a027e5c7ed1d3
Sean Lingren <sean@lingren.com>
2025-09-26 21:12:06 -1000

    cloudflare import after fix

--- terraform/cloudflare/account.tf
+++ terraform/cloudflare/account.tf
@@ -2,15 +2,12 @@ data "cloudflare_account" "account" {
   account_id = "27a6422e1d64fbe9408ab703847ecdab"
 }
 
-# once fixed, uncomment, import, find/replace data block
-# https://github.com/cloudflare/terraform-provider-cloudflare/issues/5716
-# tfim cloudflare_account.account '27a6422e1d64fbe9408ab703847ecdab'
-# resource "cloudflare_account" "account" {
-#   name = "lingrino"
-#   type = "standard"
+resource "cloudflare_account" "account" {
+  name = "lingrino"
+  type = "standard"
 
-#   settings = {
-#     enforce_twofactor   = true
-#     abuse_contact_email = "sean@lingren.com"
-#   }
-# }
+  settings = {
+    enforce_twofactor   = true
+    abuse_contact_email = "sean@lingren.com"
+  }
+}
