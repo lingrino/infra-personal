14fed75102775ee5159d2d067eee4faea9a74eae
Sean Lingren <sean@lingren.com>
2025-10-03 08:50:24 -1000

    pv

---
+++ terraform/github/repo_podvec.tf
@@ -0,0 +1,70 @@
+resource "github_repository" "podvec" {
+  name         = "podvec"
+  description  = ""
+  homepage_url = ""
+
+  visibility = "private"
+
+  has_wiki             = false
+  has_issues           = false
+  has_projects         = false
+  has_discussions      = false
+  vulnerability_alerts = true
+
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  allow_update_branch    = true
+  delete_branch_on_merge = true
+
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+}
+
+resource "github_branch" "podvec" {
+  repository = github_repository.podvec.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "podvec" {
+  repository = github_repository.podvec.name
+  branch     = github_branch.podvec.branch
+}
+
+resource "github_repository_ruleset" "podvec" {
+  name        = "main"
+  repository  = github_repository.podvec.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  rules {
+    deletion = true
+
+    required_linear_history = true
+    non_fast_forward        = true
+  }
+}
+
+resource "github_actions_repository_permissions" "podvec" {
+  repository      = github_repository.podvec.name
+  allowed_actions = "selected"
+
+  allowed_actions_config {
+    github_owned_allowed = true
+    verified_allowed     = true
+  }
+}
